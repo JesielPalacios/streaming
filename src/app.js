df0979fea03cbe42306411619d39caa0ac968bd4
Jesiel Palacios <50146090+JesielPalacios@users.noreply.github.com>
2022-01-21 06:39:14 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+// creamos un servidor http a partir de la librería de express
+const http = require("http").Server(app);
+
+// para generar una comunicación vamos a trabajar con socket.io
+const io = require("socket.io")(http);
+
+// routes
+app.use("./routes/littleZoom.routes");
+
+// donde vamos a cargar los html con lo que vamos a trabajar.
+app.use(express.static(__dirname + "/public"));
+
+module.exports = app;
