86ce7496c94c2d37a3e70338a006f78c7c01aaf3
Jesiel Palacios <50146090+JesielPalacios@users.noreply.github.com>
2022-01-21 13:43:34 -0500

    Add latest changes

--- src/app.js
+++ src/app.js
@@ -13,4 +13,13 @@ app.use(require("./routes/littleZoom.routes"));
 // donde vamos a cargar los html con lo que vamos a trabajar.
 app.use(express.static(__dirname + "/public"));
 
+// generar un evento que perita una coneción multicanal
+io.on("connection", (socket) => {
+  socket.on("stream", (myImage) => {
+    // emitir el evento a todos los socket conectados
+    // mediante una función emisora
+    socket.broadcast.emit("stream", myImage);
+  });
+});
+
 module.exports = app;
